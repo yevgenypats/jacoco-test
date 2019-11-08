9acb5f98b7eff91a05acd3495d97cff6ce432ea3
Yevgeny Pats <yp@fuzzit.dev>
2019-11-08 09:27:33 +0200

    add benchmark for fuzzing

--- src/main/java/org/yevgeny/MyMojo.java
+++ src/main/java/org/yevgeny/MyMojo.java
@@ -3,7 +3,16 @@ package org.yevgeny;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.jacoco.core.data.ExecutionData;
+import org.jacoco.core.data.ExecutionDataReader;
+import org.jacoco.core.data.IExecutionDataVisitor;
+import org.jacoco.core.data.ISessionInfoVisitor;
+import org.jacoco.core.data.SessionInfo;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.lang.reflect.*;
+import java.util.Date;
 
 class A {
     private final int a;
@@ -21,12 +30,49 @@ class A {
 @Mojo( name = "bug")
 public class MyMojo extends AbstractMojo
 {
+    private int hitCount;
+
+    private int getHitCount(final boolean[] data) {
+        int count = 0;
+        for (final boolean hit : data) {
+            if (hit) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public void execute()
     {
         try {
             Class c = Class.forName("org.jacoco.agent.rt.RT");
             Method m = c.getMethod("getAgent");
-            System.out.println("JaCoCo Agent: " + m.invoke(null));
+            Object Agent = m.invoke(null);
+            System.out.println("JaCoCo Agent: " + Agent);
+            m = Agent.getClass().getMethod("getExecutionData", boolean.class);
+            m.setAccessible(true);
+            long startTime = System.currentTimeMillis();
+            for (int i=0; i<1000; i++) {
+                this.hitCount = 0;
+                byte[] a = (byte[]) m.invoke(Agent, false);
+                InputStream is = new ByteArrayInputStream(a);
+                final ExecutionDataReader reader = new ExecutionDataReader(is);
+                reader.setSessionInfoVisitor(new ISessionInfoVisitor() {
+                    public void visitSessionInfo(final SessionInfo info) {
+                    }
+                });
+                reader.setExecutionDataVisitor(new IExecutionDataVisitor() {
+                    public void visitClassExecution(final ExecutionData data) {
+                        hitCount += getHitCount(data.getProbes());
+                    }
+                });
+                reader.read();
+                is.close();
+            }
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            System.out.println(elapsedTime);
+            System.out.println(hitCount);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
